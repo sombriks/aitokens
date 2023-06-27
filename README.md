727e8520d329feb07aa84762374479282e887b10
Leonardo Silveira <sombriks@gmail.com>
2023-06-27 10:12:05 -0300

    skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# sample issue with chat-gpt tokens
+
+Exploratory code to sample [this issue](https://github.com/TheoKanning/openai-java/issues/313#issuecomment-1609406474)
+
---
+++ src/main/java/sample/issue/tokens/CountAndAsk.java
@@ -0,0 +1,11 @@
+package sample.issue.tokens;
+
+public class CountAndAsk {
+
+    private final String OPENAI_TOKEN = System.getenv("OPENAI_TOKEN");
+    private final String OPENAI_MODEL = System.getenv("OPENAI_MODEL");
+
+
+
+
+}
